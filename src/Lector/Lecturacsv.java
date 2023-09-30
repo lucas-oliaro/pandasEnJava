3c316c9d4b4b7cf0ed4dd1accc3f9b9b68baf7c0
Lucas E. Oliaro Vera <lucasoliaro@gmail.com>
2023-09-30 20:46:18 -0300

    add: Works

--- src/Lector/Lecturacsv.java
+++ src/Lector/Lecturacsv.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Lecturacsv {
      public static void main(String[] args) {
-        String csvFilePath = "Ejercicios/src/TP_Ideas/utils/industry_sic.csv";
+        String csvFilePath = "./src/utils/industry_sic.csv";
     
         try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
             String line;
